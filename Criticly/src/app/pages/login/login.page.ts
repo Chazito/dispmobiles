caa38f7c2527f57f9ac6b42e4a73b89a956696dd
Chazito <f.nunez.acevedo@gmail.com>
2024-09-02 21:41:49 -0400

    Registro

--- Criticly/src/app/pages/login/login.page.ts
+++ Criticly/src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  form:FormGroup;
+
+  adminEmail : string = "admin@email.com";
+  adminPass : string = "admin@1234";
+
+  userEmail : string = "user@email.com";
+  userPass : string = "user@1234";
+
+  inputEmail : string = "";
+  inputPass : string = "";
+
+  constructor(private router : Router, private toastController:ToastController, private fb : FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+   }
 
   ngOnInit() {
   }
 
+  getValidUser(){
+    if((this.inputEmail != this.adminEmail && this.inputEmail != this.userEmail) ||  (this.inputPass != this.adminPass && this.inputPass != this.userPass)){
+      return false;
+    }
+
+    return true;
+  }
+
+  async presentToast(position: 'top' | 'middle' | 'bottom', mensaje:string, duracion:number) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: duracion,
+      position: position,
+    });
+
+    await toast.present();
+  }
+
+  onLoginClick(){
+    if(this.getValidUser()){
+      let context : NavigationExtras = {
+        state:{
+          userEmail : this.inputEmail,
+          userPass : this.inputPass,
+          isAdmin : this.inputEmail == this.adminEmail
+        }
+      }
+      this.presentToast("bottom","Sesión iniciada correctamente",2500);
+      this.router.navigate([''],context);
+    }
+    else{
+      this.presentToast("bottom","Credenciales invalidas",4000);
+    }
+  }
 }
--- Criticly/src/app/pages/registro/registro.page.ts
+++ Criticly/src/app/pages/registro/registro.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +9,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
+  StrongPasswordRegx: RegExp =
+    /^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{8,}$/;
 
-  constructor() { }
+  form: FormGroup;
 
-  ngOnInit() {
+  inputNombre : string = "";
+  inputApellido : string = "";
+  inputEmail : string = "";
+  inputPass : string = "";
+  inputPass2 : string = "";
+
+  constructor(private fb: FormBuilder, private router : Router, private alertController : AlertController) {
+    this.form = this.fb.group({
+      nombre: ['', [Validators.required]],
+      apellido: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [Validators.required, Validators.pattern(this.StrongPasswordRegx)],
+      ],
+      password2: [
+        '',
+        [Validators.required, Validators.pattern(this.StrongPasswordRegx)],
+      ],
+    });
   }
 
+  ngOnInit() {}
+
+  onRegistroClick(){
+    this.presentAlert("Registro completo","Recibira un correo de confirmacion al email introducido","Volver al Login");
+  }
+
+  async presentAlert(titulo:string, mensaje:string, boton:string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: mensaje,
+      buttons: [
+        {
+          text:boton,
+          cssClass: "alert-button",
+          handler: () =>{
+            this.router.navigate(['/login']);
+          }
+        }
+      ],
+      
+    });
+
+    await alert.present();
+  }
 }
