491ad1855436835cd00b530b7f5e7d8597c0c4e4
Eliu Contreras <eliucontrerasmj2@gmail.com>
2024-10-08 18:16:54 -0300

    Cambio de tipo booleano a integer

--- Criticly/src/app/services/servicebd.service.ts
+++ Criticly/src/app/services/servicebd.service.ts
@@ -14,16 +14,16 @@ import { Marcador } from './marcador';
 })
 export class ServicebdService {
 
-  database !: SQLiteObject;
+  database!: SQLiteObject;
 
-  tablaRol : string = "CREATE TABLE IF NOT EXISTS Rol (idRol INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT NOT NULL);";
-  tablaTipoTitulo : string = "CREATE TABLE IF NOT EXISTS TipoTitulo (idTipo INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT NOT NULL);";
-  
-  tablaUsuario : string = "CREATE TABLE IF NOT EXISTS Usuario (idUsuario INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT NOT NULL, apellido TEXT NOT NULL, correo TEXT NOT NULL, clave TEXT NOT NULL, fechaNacimiento DATE, avatar TEXT, telefono TEXT, reputacion REAL, id_rol INTEGER NOT NULL, FOREIGN KEY (id_rol) REFERENCES Rol(idRol));";
-  tablaTitulo : string = "CREATE TABLE IF NOT EXISTS Titulo (idTitulo INTEGER PRIMARY KEY AUTOINCREMENT, idTipoTitulo INTEGER NOT NULL, nombre TEXT NOT NULL, sinopsis TEXT, duracion TEXT, URLImagen TEXT, URLTrailer TEXT, fechaEstreno DATE, FOREIGN KEY (idTipoTitulo) REFERENCES TipoTitulo(idTipo));";
-  
-  tablaResenna : string = "CREATE TABLE IF NOT EXISTS Resenna (idResenna INTEGER PRIMARY KEY AUTOINCREMENT, idUsuario INTEGER NOT NULL, id_titulo INTEGER NOT NULL, comentario TEXT, fechaPublicacion DATE, calificacion REAL, esVisible BOOLEAN, fechaEliminada DATE, motivoEliminacion TEXT, FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario), FOREIGN KEY (id_titulo) REFERENCES Titulo(idTitulo));";
-  tablaMarcador : string = "CREATE TABLE IF NOT EXISTS Marcador (idMarcados INTEGER PRIMARY KEY AUTOINCREMENT, idUsuario INTEGER NOT NULL, idTitulo INTEGER NOT NULL, fechaMarcado DATE, FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario), FOREIGN KEY (idTitulo) REFERENCES Titulo(idTitulo));";
+  tablaRol: string = "CREATE TABLE IF NOT EXISTS Rol (idRol INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT NOT NULL);";
+  tablaTipoTitulo: string = "CREATE TABLE IF NOT EXISTS TipoTitulo (idTipo INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT NOT NULL);";
+
+  tablaUsuario: string = "CREATE TABLE IF NOT EXISTS Usuario (idUsuario INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT NOT NULL, apellido TEXT NOT NULL, correo TEXT NOT NULL, clave TEXT NOT NULL, fechaNacimiento DATE, avatar TEXT, telefono TEXT, reputacion REAL, id_rol INTEGER NOT NULL, FOREIGN KEY (id_rol) REFERENCES Rol(idRol));";
+  tablaTitulo: string = "CREATE TABLE IF NOT EXISTS Titulo (idTitulo INTEGER PRIMARY KEY AUTOINCREMENT, idTipoTitulo INTEGER NOT NULL, nombre TEXT NOT NULL, sinopsis TEXT, duracion TEXT, URLImagen TEXT, URLTrailer TEXT, fechaEstreno DATE, FOREIGN KEY (idTipoTitulo) REFERENCES TipoTitulo(idTipo));";
+
+  tablaResenna: string = "CREATE TABLE IF NOT EXISTS Resenna (idResenna INTEGER PRIMARY KEY AUTOINCREMENT, idUsuario INTEGER NOT NULL, id_titulo INTEGER NOT NULL, comentario TEXT, fechaPublicacion DATE, calificacion REAL, esVisible BOOLEAN, fechaEliminada DATE, motivoEliminacion TEXT, FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario), FOREIGN KEY (id_titulo) REFERENCES Titulo(idTitulo));";
+  tablaMarcador: string = "CREATE TABLE IF NOT EXISTS Marcador (idMarcados INTEGER PRIMARY KEY AUTOINCREMENT, idUsuario INTEGER NOT NULL, idTitulo INTEGER NOT NULL, fechaMarcado DATE, FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario), FOREIGN KEY (idTitulo) REFERENCES Titulo(idTitulo));";
 
   //variable para observable de estado de la Base de Datos
   private isDbReady: BehaviorSubject<boolean> = new BehaviorSubject(false);
@@ -39,37 +39,37 @@ export class ServicebdService {
 
   constructor(private sqlite: SQLite, private platform: Platform, private alertController: AlertController) {
     this.crearBD();
-   }
+  }
 
-  dbState(){
+  dbState() {
     return this.isDbReady.asObservable();
   }
 
-  fetchRol() : Observable<Rol[]>{
+  fetchRol(): Observable<Rol[]> {
     return this.listaRol.asObservable();
   }
 
-  fetchTipoTitulo() : Observable<TipoTitulo[]>{
+  fetchTipoTitulo(): Observable<TipoTitulo[]> {
     return this.listaTipoTitulo.asObservable();
   }
 
-  fetchUsuario() : Observable<Usuario[]>{
+  fetchUsuario(): Observable<Usuario[]> {
     return this.listaUsuario.asObservable();
   }
 
-  fetchTitulo() : Observable<Titulo[]>{
+  fetchTitulo(): Observable<Titulo[]> {
     return this.listaTitulo.asObservable();
   }
 
-  fetchResenna() : Observable<Resenna[]>{
+  fetchResenna(): Observable<Resenna[]> {
     return this.listaResenna.asObservable();
   }
 
-  fetchMarcador() : Observable<Marcador[]>{
+  fetchMarcador(): Observable<Marcador[]> {
     return this.listaMarcador.asObservable();
   }
 
-  async presentAlert(titulo:string, msj:string) {
+  async presentAlert(titulo: string, msj: string) {
     const alert = await this.alertController.create({
       header: titulo,
       message: msj,
@@ -79,28 +79,28 @@ export class ServicebdService {
     await alert.present();
   }
 
-  crearBD(){
+  crearBD() {
     //verificar si la plataforma esta lista o no
-    this.platform.ready().then(()=>{
+    this.platform.ready().then(() => {
       //creamos la base de datos
       this.sqlite.create({
         name: 'bdnoticias.db',
         location: 'default'
-      }).then((bd: SQLiteObject)=>{
+      }).then((bd: SQLiteObject) => {
         //guardar la conexion
         this.database = bd;
         //llamar a la creación de tablas
         this.crearTablas();
         //modificar el estado de mi base de datos
         this.isDbReady.next(true);
-      }).catch(e=>{
-        this.presentAlert('Creación de BD','Error: ' + JSON.stringify(e));
+      }).catch(e => {
+        this.presentAlert('Creación de BD', 'Error: ' + JSON.stringify(e));
       })
     })
   }
 
-  async crearTablas(){
-    try{
+  async crearTablas() {
+    try {
       await this.database.executeSql(this.tablaRol, []);
 
       await this.database.executeSql(this.tablaTipoTitulo, []);
@@ -113,16 +113,16 @@ export class ServicebdService {
 
       await this.database.executeSql(this.tablaMarcador, []);
 
-    }catch(e){
-      this.presentAlert('Creación de Tablas','Error: ' + JSON.stringify(e));
+    } catch (e) {
+      this.presentAlert('Creación de Tablas', 'Error: ' + JSON.stringify(e));
     }
   }
 
-  selectTipoTitulo(){
-    return this.database.executeSql("SELECT * FROM tipotitulo",[]).then(res => {
-      let items : TipoTitulo[] = [];
-      if(res.rows.length > 0){
-        for(var i = 0; i < res.rows.length; i++){
+  selectTipoTitulo() {
+    return this.database.executeSql("SELECT * FROM tipotitulo", []).then(res => {
+      let items: TipoTitulo[] = [];
+      if (res.rows.length > 0) {
+        for (var i = 0; i < res.rows.length; i++) {
           items.push({
             idTipo: res.rows.item(i).idTipo,
             nombre: res.rows.item(i).nombre
@@ -132,11 +132,11 @@ export class ServicebdService {
     })
   }
 
-  selectRol(){
-    return this.database.executeSql("SELECT * FROM rol",[]).then(res => {
-      let items : Rol[] = [];
-      if(res.rows.length > 0){
-        for(var i = 0; i < res.rows.length; i++){
+  selectRol() {
+    return this.database.executeSql("SELECT * FROM rol", []).then(res => {
+      let items: Rol[] = [];
+      if (res.rows.length > 0) {
+        for (var i = 0; i < res.rows.length; i++) {
           items.push({
             idRol: res.rows.item(i).idRol,
             nombre: res.rows.item(i).nombre
