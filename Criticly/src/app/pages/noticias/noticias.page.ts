b8f0de2892a6830a844251ddd1a4a853ba8295de
Eliu Contreras <eliucontrerasmj2@gmail.com>
2024-11-12 22:02:56 -0300

    Integrar API Nativa de navegador para noticias

--- Criticly/src/app/pages/noticias/noticias.page.ts
+++ Criticly/src/app/pages/noticias/noticias.page.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { API_KEY } from 'environment';
+import { Browser } from '@capacitor/browser';
 
 @Component({
   selector: 'app-noticias',
@@ -9,12 +10,29 @@ import { API_KEY } from 'environment';
 })
 export class NoticiasPage implements OnInit {
   noticias: any = []
+  cargandoNoticias: boolean = false
+  totalImagenes: number = 0;
+  imagenesCargadas: number = 0;
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.cargandoNoticias = true
     this.http.get("https://newsapi.org/v2/everything?q=película&language=es&apiKey=" + API_KEY).subscribe((res: any) => {
       this.noticias = res.articles.filter((f: any) => f.author !== null).slice(0, 20)
+      this.totalImagenes = this.noticias.length;
+      this.imagenesCargadas = 0;
     })
+
   }
 
+  imagenCargada() {
+    this.imagenesCargadas++;
+    if (this.imagenesCargadas >= 20) {
+      this.cargandoNoticias = false;
+    }
+  }
+
+  async navegarANoticia(url: string) {
+    await Browser.open({ url, toolbarColor: "#060B1D" })
+  }
 }
