02d949a0a8b80a8204731813d53ec007b1cc472e
Chazito <f.nunez.acevedo@gmail.com>
2024-12-06 19:19:49 -0300

    Cambio de nombre y cambio al qr y link de descarga

--- web/critical/Criticly/wsgi.py
+++ web/critical/Criticly/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for critical project.
+WSGI config for Criticly project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'critical.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Criticly.settings')
 
 application = get_wsgi_application()
